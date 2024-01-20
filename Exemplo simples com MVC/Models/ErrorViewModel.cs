7d2a0bbff0d1cf09bd605d2efacd6de6c6da9331
Senac <Senac@JLE207P060841>
2024-01-19 21:26:37 -0300

    Adicionar arquivos de projeto.

---
+++ Exemplo simples com MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Exemplo_simples_com_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
